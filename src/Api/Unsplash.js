f7104f43e8c004c23effd6745097efa8c64076c2
vikthour01 <vikthour01@gmail.com>
2022-03-16 01:48:22 -1200

    initoal commit

--- src/Api/Unsplash.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios"
-
-//axios is responsible for the api request
-export default axios.create({
-    baseURL:"https://api.unsplash.com",
-    headers:{
-        Authorization: "Client-ID e0dp_vYc1f-5J1Vp72-hACtE04kB7RTzyqL5iJWNpko"
-    } 
-})
